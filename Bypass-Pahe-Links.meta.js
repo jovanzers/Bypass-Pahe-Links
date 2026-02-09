02119c770a4a464f8aeb055598d91ea1ba169ab4
Jovan Ferryal E. F. <jovanzers@gmail.com>
2026-02-09 14:46:10 +0700

    Add metadata for Bypass Pahe Links userscript

---
+++ Bypass-Pahe-Links.meta.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Bypass Pahe Links
+// @namespace    https://github.com/jovanzers
+// @version      0.103
+// @description  Simplify link jumping on pahe ad websites.
+// @author       NaeemBolchhi & Jovanzers
+// @match        http*://teknoasian.com/*
+// @match        http*://intercelestial.com/*
+// @match        http*://linegee.net/*
+// @match        http*://spacetica.com/*
+// @match        http*://pahe.plus/*
+// @match        http*://oii.la/*
+// @match        http*://uii.io/*
+// @match        http*://wp2hostt.com/*
+// @match        http*://wordcounter.icu/*
+// @match        http*://tpi.li/*
+// @match        http*://blogmystt.com/*
+// @match        http*://old.pahe.plus/*
+// @match        http*://hosttbuzz.com/*
+// @match        http*://policiesreview.com/*
+// @match        http*://healthylifez.com/*
+// @match        http*://insurancemyst.com/*
+// @match        http*://*.gdflix.dad/file/*
+// @match        http*://pixeldrain.com/*
+// @match        http*://hostingbixby.com/*
+// @match        http*://policiesbuzzz.com/*
+// @match        http*://hostingzbuzz.com/*
+// @match        http*://bixbyfortech.com/*
+// @match        http*://serverguidez.com/*
+// @match        http*://comparepolicyy.com/*
+// @match        http*://cheaplann.com/*
+// @match        http*://vpshostplans.com/*
+// @match        http*://ensureguide.com/*
+// @match        http*://fitnessplanss.com/*
+// @match        http*://sharedwebs.com/*
+// @license      GPL-3.0-or-later
+// @require      https://greasyfork.org/scripts/456228/code/456228.js?version=1309113
+// @run-at       document-body
+// @grant        none
+// ==/UserScript==
